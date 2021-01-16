eb4d4248c4b26098eceac1e802dc784efa8106bb
nurisis <isoyeong666@naver.com>
2021-01-16 11:51:07 +0900

    Content provider simple example

--- app/src/main/java/com/example/androidcomponentsexam/ContentProviderActivity.kt
+++ app/src/main/java/com/example/androidcomponentsexam/ContentProviderActivity.kt
@@ -1,11 +1,35 @@
 package com.example.androidcomponentsexam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.UserDictionary
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class ContentProviderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content_provider)
+
+        val projectionString = arrayOf(
+            UserDictionary.Words._ID,
+            UserDictionary.Words.WORD,
+            UserDictionary.Words.LOCALE
+        )
+
+        val selectClause: String? = null
+
+        val selectArgs: Array<String>? = null
+
+        val order: String? = null
+
+        // Queries the user dictionary and returns results
+        val cursor = contentResolver.query(
+            UserDictionary.Words.CONTENT_URI,   // The content URI of the words table
+            projectionString,                        // The columns to return for each row
+            selectClause,                   // Selection criteria
+            selectArgs,      // Selection criteria
+            order                          // The sort order for the returned rows
+        )
+
     }
 }
